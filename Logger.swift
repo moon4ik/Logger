4074ce7a566bb93279e7f417e7bd88f63917cccd
Oleksii <moon4ik@gmail.com>
2018-12-04 16:56:40 +0200

    Update Logger.swift

--- Logger.swift
+++ Logger.swift
@@ -9,6 +9,21 @@
 
 import Foundation
 
+//  
+//  Logger is writing your logs into the files 
+//  
+//  If you want read log files from the mobile using standart app Files or by connecting mobile phone to iTunes, you need to add to Key into your "Info.plist"
+//  
+//  Supports opening documents in place - YES
+//  
+//  <key>LSSupportsOpeningDocumentsInPlace</key>
+//  <true/>
+//  Application supports iTunes file sharing - YES
+//  
+//  <key>UIFileSharingEnabled</key>
+//  <true/>
+//  
+
 struct Logger {
     
     /// return date formatter
